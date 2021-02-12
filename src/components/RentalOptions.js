9390643624ed6b0981cf9ae25944aff64088ccc0
Sarah Rainsberger <sarah@rainsberger.ca>
2021-02-12 12:36:51 +0000

    Reformatted rental property options display to include the monthly rent. (NB: will eventually use this amount to pre-populate amount collected)

--- src/components/RentalOptions.js
+++ src/components/RentalOptions.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function RentalOptions() {
     const rentalProperties = [
-    {address: '123 Main Street', renter: 'Sarah', montlyRent: 1000 },
+    {address: '123 Main Street', renter: 'Sarah', monthlyRent: 1000 },
     {address: '456 Electric Avenue', renter: 'J.B.', monthlyRent: 950},
-    {address: '789 Penny Lane', renter: 'Max', montlyRent: 800}
+    {address: '789 Penny Lane', renter: 'Max', monthlyRent: 800}
     ]
   return (
     rentalProperties.map( (property) =>
-        <option value={property.address}>{property.address} - {property.renter}</option> 
+        <option value={property.address}>{property.address} ({property.renter}) - ${property.monthlyRent}</option> 
     )
   )   
 }
