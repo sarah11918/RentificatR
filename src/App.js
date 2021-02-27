c954bac1a9b95066875b35c1d5dc106efdab8cac
Sarah Rainsberger <sarah@rainsberger.ca>
2021-02-27 20:56:53 +0000

    made summary and collection form protected routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import useAuthListener from './hooks/use-auth-listener';
 import * as ROUTES from './constants/routes';
 import UserContext from './context/user'
+import ProtectedRoute from './helpers/protected-route'
 
 
 const CollectionForm = lazy(() => import ('./pages/collection-form'));
@@ -21,9 +22,13 @@ function App() {
     <Router>
       <Suspense fallback = {<p>Loading...</p>}>
         <Switch>
-          <Route path={ROUTES.COLLECTION_FORM} component={CollectionForm} exact />
+          <ProtectedRoute user={user} path={ROUTES.COLLECTION_FORM} exact >
+            <CollectionForm />
+          </ProtectedRoute>
           <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SUMMARY} component={Summary} />
+          <ProtectedRoute user={user} path={ROUTES.SUMMARY}>
+            <Summary />
+          </ProtectedRoute>
           <Route component={NotFound} />
         </Switch>
       </Suspense>
